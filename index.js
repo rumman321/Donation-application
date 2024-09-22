1a300d525451ce431a96b7456153ff25f2bf3b8a
rumman321 <cset-210652@dti.ac>
2024-09-23 05:49:42 +0600

    card 2 donation for flood  at Feni complete with functional

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ document.getElementById('donate-btn1')
     document.getElementById('total-donation').innerText=totalAmount.toFixed(2)
     
 })
+// donation for flood  at Feni
+document.getElementById('donate-btn2')
+.addEventListener('click',function(){
+    const amount1=parseFloat(document.getElementById('donate-amount2').value)
+    console.log(amount1)
+
+    const amount=parseFloat(document.getElementById('total-donation').innerText)
+    const totalAmount=amount+amount1
+    document.getElementById('total-donation').innerText=totalAmount.toFixed(2)
+    
+})
