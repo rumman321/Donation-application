0d7e2a8c9a4d74d9d7bdda2aca4d39fb048cb4ff
rumman321 <cset-210652@dti.ac>
2024-09-23 05:43:05 +0600

    card 1 donation for flood at noakhali js  complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// donation for flood at noakhali
+document.getElementById('donate-btn1')
+.addEventListener('click',function(){
+    const amount1=parseFloat(document.getElementById('donate-amount1').value)
+    console.log(amount1)
 
+    const amount=parseFloat(document.getElementById('total-donation').innerText)
+    const totalAmount=amount+amount1
+    document.getElementById('total-donation').innerText=totalAmount.toFixed(2)
+    
+})
