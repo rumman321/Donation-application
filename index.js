ad9d2a5141fafb1d9909a9993723969b099edf2f
rumman321 <cset-210652@dti.ac>
2024-09-23 06:10:06 +0600

    card 3 Aid for Injured in the Quota Movement complete with functional

--- index.js
+++ index.js
@@ -2,21 +2,29 @@
 document.getElementById('donate-btn1')
 .addEventListener('click',function(){
     const amount1=parseFloat(document.getElementById('donate-amount1').value)
-    console.log(amount1)
-
     const amount=parseFloat(document.getElementById('total-donation').innerText)
     const totalAmount=amount+amount1
     document.getElementById('total-donation').innerText=totalAmount.toFixed(2)
+    document.getElementById('donate-amount1').value=''
     
 })
 // donation for flood  at Feni
 document.getElementById('donate-btn2')
 .addEventListener('click',function(){
     const amount1=parseFloat(document.getElementById('donate-amount2').value)
-    console.log(amount1)
-
     const amount=parseFloat(document.getElementById('total-donation').innerText)
     const totalAmount=amount+amount1
     document.getElementById('total-donation').innerText=totalAmount.toFixed(2)
+    document.getElementById('donate-amount2').value=''
+    
+})
+// Aid for Injured in the Quota Movement
+document.getElementById('donate-btn3')
+.addEventListener('click',function(){
+    const amount1=parseFloat(document.getElementById('donate-amount3').value)
+    const amount=parseFloat(document.getElementById('total-donation').innerText)
+    const totalAmount=amount+amount1
+    document.getElementById('total-donation').innerText=totalAmount.toFixed(2)
+    document.getElementById('donate-amount3').value=''
     
 })
